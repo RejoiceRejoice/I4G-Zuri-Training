92be65b16708c3f0cbbc09f6f7f736e252bb52bd
RejoiceRejoice <105215752+RejoiceRejoice@users.noreply.github.com>
2022-05-28 22:20:50 +0100

    Add files via upload
    Finding anagrams

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
+from tkinter import scrolledtext
 
 
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
-    if (sorted(word) == sorted(anagram)):
-        print(sorted(word), sorted(anagram))
-        return True
-    return False    
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    Detail='Student'
 
-print(find_anagram('hello', 'check'))
+    def __init__(self, name, age, tracks, score):
+        self.name= str(name)
+        self.age= int(age)
+        self.tracks= list(tracks)
+        self.score= float(score)
+    
+    def change_name(self, new_name):
+        self.change_name = new_name
+        new_name = 'Peter'
+        print(f'The student updated his name to {new_name}')
 
-def find_anagram(word, anagram):
-    if (sorted(word) == sorted(anagram)):
-        print(sorted(word), sorted(anagram))
-        return True
-    return False    
+    def change_age(self, new_age):
+        self.change_age =new_age
+        new_age = '34'
+        print(f'The student updated his age to {new_age}')    
 
-print(find_anagram('below', 'elbow'))
+    def add_track(self, new_track):
+        self.add_track = new_track
+        new_track = ('UI/UX')
+        print(f'The student updated his track records to {new_track}')    
+
+    def get_score(self, new_score):
+        self.get_score = new_score
+        new_score = 30.9
+        print(f'The student updated his score to {new_score}')
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score(30.90)
